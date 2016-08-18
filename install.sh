7192f93861c366a88793accc46102e804aa1ba17
Haipeng <njucs_wuhaipeng@163.com>
2016-08-18 15:15:12 +0800

    quiet install

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ set -e
 
 [ "xxx$1" != "xxx" ] && version=":$1"
 
-bash -c "$(docker run --rm daocloud.io/daocloud/dce${version} install)"
+bash -c "$(docker run --rm -i daocloud.io/daocloud/dce${version} install -q)"
