1100952ea6495455f099191326ef11553e54175a
Haipeng <njucs_wuhaipeng@163.com>
2016-08-18 14:56:00 +0800

    support dce 2.0

--- demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-function dd(){
-    echo $1
-    echo $2
-    echo $3
-}
-
-dd `sed -n 1p dce.conf`
--- set_nopasswd_login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-host=$1
-username=${2:-root}
-password=${3:-''}
-
-./set_nopasswd_login.exp $host $username $password
