21a793a9653dd0b3c40c2fe8b39f9bd59eed4556
Haipeng <njucs_wuhaipeng@163.com>
2016-08-29 10:42:53 +0800

    improve install and uninstall script

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ set -e
 
 [ "xxx$1" != "xxx" ] && version=":$1"
 
+service docker restart
 bash -c "$(docker run --rm -i daocloud.io/daocloud/dce${version} install -q | sed "s/-it/-i/g")"
--- uninstall.sh
+++ uninstall.sh
@@ -2,5 +2,7 @@
 
 docker swarm leave --force > /dev/null 2>&1
 docker ps -a | awk '{print $1}' | grep -v "CONTAINER" | xargs docker rm -vf > /dev/null 2>&1
+
 rm -rf /var/local/dce
 rm -rf /etc/daocloud/dce
+rm -rf /etc/docker/daemon.json
